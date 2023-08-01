f8390e9e8458d901bb48d777aa0e874c81053c71
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2023-08-01 18:36:59 +0200

    json variable source

--- components/providers/scenario/vs_json.go
+++ components/providers/scenario/vs_json.go
@@ -1,14 +1,18 @@
 package scenario
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+
 	"github.com/spf13/afero"
 )
 
 type VariableSourceJson struct {
-	Name    string         `yaml:"name"`
-	File    string         `yaml:"file"`
-	Mapping map[string]any `yaml:"mapping"`
-	fs      afero.Fs
+	Name  string `yaml:"name"`
+	File  string `yaml:"file"`
+	fs    afero.Fs
+	store any
 }
 
 func (v *VariableSourceJson) GetName() string {
@@ -16,11 +20,35 @@ func (v *VariableSourceJson) GetName() string {
 }
 
 func (v *VariableSourceJson) GetVariables() any {
-	return v.Mapping
+	return v.store
 }
 
-func (v *VariableSourceJson) Init() error {
-	panic("implement me")
+func (v *VariableSourceJson) Init() (err error) {
+	const op = "VariableSourceJson.Init"
+	var file afero.File
+	file, err = v.fs.Open(v.File)
+	if err != nil {
+		return fmt.Errorf("%s fs.Open %w", op, err)
+	}
+	defer func() {
+		closeErr := file.Close()
+		if closeErr != nil {
+			if err != nil {
+				err = fmt.Errorf("%s multiple errors faced: %w, with close err: %s", op, err, closeErr)
+			} else {
+				err = fmt.Errorf("%s, %w", op, closeErr)
+			}
+		}
+	}()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("%s io.ReadAll %w", op, err)
+	}
+	err = json.Unmarshal(data, &v.store)
+	if err != nil {
+		return fmt.Errorf("%s readCsv %w", op, err)
+	}
+
 	return nil
 }
 
