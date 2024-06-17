c9d0626a2c0e1666e0a8d9fdab3d3913443dee2d
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2024-06-17 19:16:51 +0200

    release v0.5.27.alpha3 - try parse imbalance

--- cli/cli.go
+++ cli/cli.go
@@ -26,7 +26,7 @@ import (
 	"github.com/yandex/pandora/lib/zaputil"
 )
 
-const Version = "0.5.27.alpha2"
+const Version = "0.5.27.alpha3"
 const defaultConfigFile = "load"
 const stdinConfigSelector = "-"
 
