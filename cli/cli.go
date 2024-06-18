1b61d091afcea93ffa07655d0be0f0683e73d4aa
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2024-06-18 17:28:23 +0200

    v0.5.27.alpha15

--- cli/cli.go
+++ cli/cli.go
@@ -26,7 +26,7 @@ import (
 	"github.com/yandex/pandora/lib/zaputil"
 )
 
-const Version = "0.5.27.alpha14"
+const Version = "0.5.27.alpha15"
 const defaultConfigFile = "load"
 const stdinConfigSelector = "-"
 
